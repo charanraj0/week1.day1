0811c40f465530889b90bfefddf616779e160b65
charanraj0 <107326442+charanraj0@users.noreply.github.com>
2022-06-11 17:59:16 -0400

    Add files via upload

---
+++ primeNumber.java
@@ -0,0 +1,19 @@
+package sajan;
+
+import java.util.Iterator;
+
+public class primeNumber {
+public static void main(String[] args) {
+	int n = 13;
+	boolean flag = false;
+	for (int i = 2; i <=n/2; i++) {
+		if (n%i==0) 
+			flag=true;
+			break;
+	}
+	if (!flag)
+	      System.out.println(n + " is a prime number.");
+	    else
+	      System.out.println(n + " is not a prime number.");
+}
+}
